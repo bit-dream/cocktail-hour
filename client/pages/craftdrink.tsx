ef8b5c5afabc73ff810985dc8c98b6c052fd7a11
derek <derekblackj@gmail.com>
2022-04-17 18:53:00 -0400

    Added temp page for crafted cocktail

---
+++ client/pages/craftdrink.tsx
@@ -0,0 +1,68 @@
+import type { NextPage } from 'next'
+import { Box, Flex, Spacer, Text } from '@chakra-ui/react'
+import { Center, Container, Heading, List, ListIcon, ListItem } from '@chakra-ui/react'
+import { Input, InputGroup, InputLeftElement, InputRightElement, Button } from '@chakra-ui/react'
+import { Search2Icon, ArrowForwardIcon } from '@chakra-ui/icons'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Fade, ScaleFade, Slide, SlideFade } from '@chakra-ui/react'
+
+const CraftDrink: NextPage = () => {
+  return (
+    <Flex justify='center' align='center' height='100vh'>
+      <ScaleFade initialScale={0.50} in={true}>
+        <Container maxW='1000px' boxShadow='dark-lg' p='6' rounded='md' bg='white'>
+        <VStack
+        spacing={4}
+        align='stretch'
+        >
+          <Center>
+            <Text
+              bgGradient='linear(to-l, #7928CA, #FF0080)'
+              bgClip='text'
+              fontSize='5xl'
+              fontWeight='extrabold'
+            >
+              The Saosin
+            </Text>
+          </Center>
+          <Spacer/>
+          <Spacer/>
+
+        <Stack spacing={8} direction='row'>
+          <Box boxShadow='md' p='6' rounded='md' bg='white'>
+            <Heading fontSize='xl'>What you'll need</Heading>
+            <List spacing={3}>
+              <ListItem>
+                <ListIcon as={ArrowForwardIcon} color='green.500' />
+                Tequila
+              </ListItem>
+              <ListItem>
+                <ListIcon as={ArrowForwardIcon} color='green.500' />
+                Seltzer
+              </ListItem>
+              <ListItem>
+                <ListIcon as={ArrowForwardIcon} color='green.500' />
+                Cherry
+              </ListItem>
+              <ListItem>
+                <ListIcon as={ArrowForwardIcon} color='green.500' />
+                Orange
+              </ListItem>
+            </List>
+          </Box>
+          <Box boxShadow='md' p='6' rounded='md' bg='white'>
+            <Heading fontSize='xl'>How to make it</Heading>
+            <Text mt={4}>Start by combining tequila and seltzer in a cocktail shaker. Muddle cherry and orange. Shake vigerously until chilled. Serve in a coup class. Garnish with lime wedge.</Text>
+          </Box>
+        </Stack>
+        <Button colorScheme='teal' variant='outline'>
+          Craft Another Cocktail
+        </Button>
+        </VStack>
+        </Container>
+      </ScaleFade>
+    </Flex>
+  )
+}
+
+export default CraftDrink
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,46 +1,52 @@
 import type { NextPage } from 'next'
 import { Box, Flex, Spacer, Text } from '@chakra-ui/react'
 import { Center, Container } from '@chakra-ui/react'
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import { Search2Icon } from '@chakra-ui/icons'
+import { Input, InputGroup, InputLeftElement, InputRightElement, Button } from '@chakra-ui/react'
+import { Search2Icon, ArrowForwardIcon } from '@chakra-ui/icons'
 import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Fade, ScaleFade, Slide, SlideFade } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
     <Flex justify='center' align='center' height='100vh'>
-      <Container maxW='container.sm' boxShadow='dark-lg' p='6' rounded='md' bg='white'>
-      <VStack
-      spacing={4}
-      align='stretch'
-      >
-        <Spacer/>
-        <Center>
-          <Text
-            bgGradient='linear(to-l, #7928CA, #FF0080)'
-            bgClip='text'
-            fontSize='5xl'
-            fontWeight='extrabold'
-          >
-            Cocktail Hour
+      <ScaleFade initialScale={0.50} in={true}>
+        <Container maxW='container.sm' boxShadow='dark-lg' p='6' rounded='md' bg='white'>
+        <VStack
+        spacing={4}
+        align='stretch'
+        >
+          <Spacer/>
+          <Center>
+            <Text
+              bgGradient='linear(to-l, #7928CA, #FF0080)'
+              bgClip='text'
+              fontSize='5xl'
+              fontWeight='extrabold'
+            >
+              Cocktail Hour
+            </Text>
+          </Center>
+          <Spacer/>
+          <Spacer/>
+          <Text fontSize='lg'>
+            Craft custom and unique cocktails 🍸 based on your favorite artist 👨‍🎤, album 💽, mood 😎, or event 🎉.
           </Text>
-        </Center>
-        <Spacer/>
-        <Spacer/>
-        <Text fontSize='lg'>
-          Craft custom and unique cocktails 🍸 based on your favorite artist 👨‍🎤, album 💽, mood 😎, or event 🎉.
-        </Text>
-        <Center>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents='none'
-              children={<Search2Icon color='gray.300' />}
-            />
-            <Input type='tel' placeholder='Enter Artist or Album Name' />
-          </InputGroup>
-        </Center>
-        <Spacer/>
-      </VStack>
-    </Container>
+          <Center>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents='none'
+                children={<Search2Icon color='gray.300' />}
+              />
+              <Input type='tel' placeholder='Enter Artist or Album Name' />
+              <InputRightElement width='4.5rem'>
+                <Button colorScheme='blue' h='1.75rem' size='sm'>Craft</Button>
+              </InputRightElement>
+            </InputGroup>
+          </Center>
+          <Spacer/>
+        </VStack>
+        </Container>
+      </ScaleFade>
     </Flex>
   )
 }
