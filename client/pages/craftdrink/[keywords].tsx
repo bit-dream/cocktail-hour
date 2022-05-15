02126da9d6a5797cfc994322e990b32ec4414a94
derek <derekblackj@gmail.com>
2022-05-15 15:25:53 -0400

    minor updates

--- client/pages/craftdrink/[keywords].tsx
+++ client/pages/craftdrink/[keywords].tsx
@@ -27,7 +27,7 @@ const CraftDrink: NextPage = () => {
     // declare the async data fetching function
     const fetchData = async () => {
 
-      const response = await fetch(`https://localhost:5000/craftdrink?search=${search}`)
+      const response = await fetch(`https://cocktailhour.tk:5000/craftdrink?search=${search}`)
       const json: ApiResponse = await response.json()
       
       if (!validateReponse(json)) {
